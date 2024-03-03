ee1841df23317dcafdb7251d79e0f20a4c0440d0
Jefferson Lim <jefferer@gmail>
2024-03-02 18:16:05 -0800

    [auth] added authController and routes, jwt, verifyJwt middleware, loginLimiter, and authorization for users and referral routes

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
+    'http://localhost:5173',
     'https://www.sellanycondition.com',
     'https://sellanycondition.com'
 ]
--- routes/referralRoutes.js
+++ routes/referralRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const referralController = require('../controllers/referralController')
+const verifyJWT = require('../middleware/verifyJWT')
+
+router.use(verifyJWT)
 
 router.route('/')
     .get(referralController.getAllReferrals)
