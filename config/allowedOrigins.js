2a3019ae62d9857f75cc54a564a30453d0117b0c
Jefferson Lim <jefferer@gmail>
2024-03-06 18:08:27 -0800

    [deploy] update allowed origins to test deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
-    // 'http://localhost:5173',
-    'https://referral-app-0fp1.onrender.com',
+    'http://localhost:5173',
+    'https://referral-react-app.onrender.com',
+    'https://dev-referral-react-app.onrender.com',
+    'https://stage-referral-react-app.onrender.com'
 ]
 
 module.exports = allowedOrigins
